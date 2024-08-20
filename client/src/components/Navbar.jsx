b14cbbaa77092a677f17cd8b648ca7241978d627
Atharva Lakhamade. <102431866+atha2810@users.noreply.github.com>
2024-08-20 19:09:33 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
 
   const handleLogout = () => {
     axios
-      .get("https://readright-server.onrender.com/auth/logout", { withCredentials: true }) // Include credentials with the request
+      .get("https://read-right-8057snk87-atha2810s-projects.vercel.app/auth/logout", { withCredentials: true }) // Include credentials with the request
       .then(res => {
         if (res.data.logout) {
           // Clear any client-side state if needed
