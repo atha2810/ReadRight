804f8090dde04908c18a191ada5445edc076c0f2
Atharva Lakhamade. <102431866+atha2810@users.noreply.github.com>
2024-08-21 22:29:46 +0530

    Update Dashboard.jsx

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard() {
     e.preventDefault();
     const token = Cookies.get("authToken");
     try {
-      const response = await fetch("/books", {
+      const response = await fetch("https://readright-server.onrender.com/books", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
