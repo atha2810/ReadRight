cf5a6a93e1e79ae0de57c5200b24e4881c0305ae
Atharva Lakhamade. <102431866+atha2810@users.noreply.github.com>
2024-08-21 22:49:39 +0530

    Update BookCategories.jsx

--- client/src/components/BookCategories.jsx
+++ client/src/components/BookCategories.jsx
@@ -38,7 +38,7 @@ function BookCategories() {
 
   useEffect(() => {
     if (selectedCategory) {
-      fetch(`/books/category/${selectedCategory}`)
+      fetch(`https://readright-server.onrender.com/books/category/${selectedCategory}`)
         .then((response) => response.json())
         .then((data) => setBooks(data))
         .catch((error) => {
